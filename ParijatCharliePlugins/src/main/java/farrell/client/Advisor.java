958587c38ea6ce3329e70c0e661fc1daf29ac569
DasParijat <143519231+DasParijat@users.noreply.github.com>
2026-03-12 11:23:47 -0400

    Did Part3 of Lab6

--- ParijatCharliePlugins/src/main/java/farrell/client/Advisor.java
+++ ParijatCharliePlugins/src/main/java/farrell/client/Advisor.java
@@ -6,7 +6,9 @@ import charlie.plugin.IAdvisor;
 import charlie.util.Play;
 
 public class Advisor implements IAdvisor {
-    public Play advise(Hand var1, Card var2) {
-        return Play.NONE;
+    BasicStrategy bs = new BasicStrategy();
+
+    public Play advise(Hand hand, Card upCard) {
+        return bs.getPlay(hand, upCard);
     }
 }
